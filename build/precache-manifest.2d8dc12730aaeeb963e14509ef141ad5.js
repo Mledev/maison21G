e1d41fb881de44ccd985068b5e7ca4b5eb0c66e2
Matthieu Le Dévéhat <mledevehat@voxsquare.com>
2019-06-11 12:08:13 +0200

    production

---
+++ build/precache-manifest.2d8dc12730aaeeb963e14509ef141ad5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d0611908f90a69d13211611b247996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "861d308ce0ef84fdd362",
+    "url": "/static/css/main.4af6797b.chunk.css"
+  },
+  {
+    "revision": "88ee071bf46fc6b893e6",
+    "url": "/static/js/2.7ad91dd1.chunk.js"
+  },
+  {
+    "revision": "861d308ce0ef84fdd362",
+    "url": "/static/js/main.60eb6eb2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4d83aa9a3515083eacaf0758bcb6697b",
+    "url": "/static/media/home.4d83aa9a.mp4"
+  },
+  {
+    "revision": "99ab5394dc08a67d6f4a6a09817c18c0",
+    "url": "/static/media/logo.99ab5394.svg"
+  }
+]);
